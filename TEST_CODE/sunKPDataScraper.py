9568ddff77889e753329bf23ac2b16d08fa1ab71
Chris Bennett <chrisbennett@fuckmesideways.lan>
2018-08-26 21:50:56 +0100

    Working on way to find the current kp value automatically

--- TEST_CODE/sunKPDataScraper.py
+++ TEST_CODE/sunKPDataScraper.py
@@ -7,10 +7,8 @@ kpValue = ""
 maxJsonvVal = 0
 
 #get the current time but just the hour part
-timeNow = datetime.datetime.now().time().hour
+#timeNow = datetime.datetime.now().time().hour
 
-timeNowTest = datetime.datetime.now()
-print(timeNowTest)
 
 #url to the json file
 kpData = "http://services.swpc.noaa.gov/products/noaa-planetary-k-index.json"
@@ -18,6 +16,11 @@ kpData = "http://services.swpc.noaa.gov/products/noaa-planetary-k-index.json"
 url = requests.get(kpData)
 #load the json file in a text format
 kp = json.loads(url.text)
+
+for line in url:
+    maxJsonValue = maxJsonvVal + 1
+
+print(maxJsonvVal)
 
 kpValue = (kp[maxJsonvVal][1])
 
